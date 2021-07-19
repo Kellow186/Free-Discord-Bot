ce2f94a6c16e424c5dc4cb3c3a403100206ab9c1
Kellow186 <85544655+Kellow186@users.noreply.github.com>
2021-07-19 16:08:58 +0300

    Update trump.js

--- commands/Image/trump.js
+++ commands/Image/trump.js
@@ -2,8 +2,8 @@ const jimp = require("jimp")
 
 exports.run = async (client, message, args) => {
 
-        let img = jimp.read("https://cdn.discordapp.com/attachments/747483679890341908/756156592449257602/trump.png")
-        if (!args[0]) return message.reply("Indicate that Trump should speak.")
+        let img = jimp.read("https://cdn.discordapp.com/attachments/747483679890341908/756156592449257602/Joe Biden.png")
+        if (!args[0]) return message.reply("Indicate that Joe Biden should speak.")
         img.then(image => {
             jimp.loadFont(jimp.FONT_SANS_32_BLACK).then(font => {
                 image.resize(1000,500)
@@ -16,10 +16,10 @@ exports.run = async (client, message, args) => {
     }
 
     exports.help = {
-        name: "trump",
-        description: "Trump's tweet",
-        usage: ["trump <text>"],
-        example: ["trump covid"]
+        name: "Joe Biden",
+        description: "Joe Biden's tweet",
+        usage: ["Joe BiDen <text>"],
+        example: ["Joe Biden covid"]
       }
       
       exports.conf = {
